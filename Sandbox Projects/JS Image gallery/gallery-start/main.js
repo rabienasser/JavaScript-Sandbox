6ba869d35b3ca19f877da52a21ceeb51f7451307
Rabie Nasser <robienasser13@gmail.com>
2021-03-13 13:53:08 -0500

    sandbox update

---
+++ Sandbox Projects/JS Image gallery/gallery-start/main.js
@@ -0,0 +1,30 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+for (let i = 1; i <= 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    thumbBar.appendChild(newImage);
+    newImage.onclick = function(event){
+        displayedImage.src = e.target.src;
+    }
+}
+
+btn.onclick = function() {
+    const btnClass = btn.getAttribute('class');
+    if(btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  }
+
+/* Wiring up the Darken/Lighten button */
