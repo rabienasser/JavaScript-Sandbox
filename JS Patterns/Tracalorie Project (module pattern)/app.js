e6de662746641bc9ee23c1a4cf5b060b60974156
Rabie Nasser <robienasser13@gmail.com>
2021-04-01 23:24:15 -0400

    sandbox update

---
+++ JS Patterns/Tracalorie Project (module pattern)/app.js
@@ -0,0 +1,52 @@
+// Storage Controller
+
+// Item Controller
+const ItemCtrl = (function() {
+    // Item Constructor
+    const Item = function(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    // Data Structure
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 1, name: 'Fruit & Protein Smoothie', calories: 400},
+            {id: 2, name: 'Lentil Soup', calories: 300},
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+    
+    return {
+        getItems: function() {
+            return data;
+        }
+        logData: function(){
+            return data;
+        }
+    }
+
+})();
+
+
+// UI Controller
+const UICtrl = (function() {
+
+})();
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl) {
+
+    return {
+        init: function(){
+            console.log('Initializing App...')
+        }
+    }
+
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init()
