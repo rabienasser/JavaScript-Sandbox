f8f967e9560bbbc732f3f872b5141c92414c844c
Rabie Nasser <robienasser13@gmail.com>
2021-04-15 18:02:24 -0400

    bricks added

--- Projects Course/17 Breakout Game/app.js
+++ Projects Course/17 Breakout Game/app.js
@@ -6,6 +6,21 @@ const ctx = canvas.getContext('2d');
 
 let score = 0;
 
+const brickRowCount = 5;
+const brickColumnCount = 9;
+
+
+// ITEM PROPERTIES
+// Create Brick Props
+const  brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
 // Create Ball props
 const ball = {
     x: canvas.width / 2,
@@ -26,6 +41,21 @@ const paddle = {
     dx: 0
 }
 
+
+//DRAWING ITEMS IN UI
+
+// Create Bricks
+const bricks = [];
+for(let i = 0; i < brickColumnCount; i++) {
+    bricks[i] = [];
+    for(let j = 0; j < brickRowCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
+    console.log(bricks[i]);
+}
+
 // Draw ball onto canvas
 function drawBall() {
     ctx.beginPath();
@@ -44,17 +74,30 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+// Draw score on canvas
+function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+// Draw Bricks on Canvas
+function drawBricks() {
+    bricks.forEach((column) => {
+        column.forEach((brick) => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#7584cb' : 'transparent';
+            ctx.fill();
+            ctx.closePath();
+        })
+    })
+}
 // Draw Everything
 function draw() {
     drawBall();
     drawPaddle();
     drawScore();
-}
-
-// Draw score on canvas
-function drawScore() {
-    ctx.font = '20px Arial';
-    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+    drawBricks();
 }
 
 draw();
