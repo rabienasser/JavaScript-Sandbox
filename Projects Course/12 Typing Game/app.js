d02a412921a08ebe3841cec519509f1a47a37686
Rabie <rabienasser2@gmail.com>
2021-04-21 22:49:36 -0400

    start/end buttons

--- Projects Course/12 Typing Game/app.js
+++ Projects Course/12 Typing Game/app.js
@@ -1,5 +1,6 @@
 const difficuly = document.querySelector('#difficulty');
 const settingsBtn = document.querySelector('#settings-btn');
+const startGameBtn = document.querySelector('.start-game');
 const settings = document.querySelector('#settings');
 const settingsForm = document.querySelector('#settings-form');
 const word = document.querySelector('#word');
@@ -9,25 +10,53 @@ const time = document.querySelector('#time');
 const endGame = document.querySelector('#end-game-container');
 
 
+// Start game
+function startGame() {
+    let countdown = 6;
+
+    const beginGame = setInterval(function() {
+        countdown--;
+        word.innerHTML = countdown;
+        if(countdown <= 0){
+            clearInterval(beginGame);
+            setInterval(getWords, 3000);
+        }
+    }, 1000)
+
+}
+
+
 // List of words for Game
 async function getWords() {
     const response = await fetch('https://random-word-api.herokuapp.com/word?number=100');
     const data = await response.json();
-    let randomWord = Math.floor(Math.random() * data.length)
+
+    let randomWord;
 
     data.forEach(word => {
-         return (data[randomWord]);
+        randomWord = Math.floor(Math.random() * data.length)
     })
 
-    displayWord(randomWord);
+    word.innerHTML = data[randomWord];
+
+    startGameBtn.classList.remove('start-game');
+    startGameBtn.classList.add('end-game');
+    startGameBtn.textContent = 'End Game';
 }
 
-function displayWord(randomWord) {
-    word.innerHTML = randomWord;
-}
+
+// function displayWord(data) {
+
+//     data.forEach(word => {
+//         let randomWord = Math.floor(Math.random() * data.length)
+//         console.log(randomWord)
+//     })
+// }
 
 
-settingsBtn.addEventListener('click', displayWord);
+// settingsBtn.addEventListener('click', getWords);
+startGameBtn.addEventListener('click', startGame)
+
 
 
 
