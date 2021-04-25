4d1edd68f8dad1199fd69253fb32ed87e3f2c5a0
Rabie <robienasser13@gmail.com>
2021-04-25 17:01:43 -0400

    income and expense calculation

--- Projects Course/9 Expense Tracker/app.js
+++ Projects Course/9 Expense Tracker/app.js
@@ -1,20 +1,23 @@
-const balance = document.querySelector('.balance');
+const balance = document.querySelector('.total-bal');
 const income = document.querySelector('.income');
 const expense = document.querySelector('.expense');
 const items = document.querySelector('#items')
 const text = document.querySelector('#text');
 const amount = document.querySelector('#amount');
 const addBtn = document.querySelector('button');
+const incomeBal = document.querySelector('.in-bal')
+const expenseBal = document.querySelector('.ex-bal')
 
+// ARRAYS HOLDING TRANSACTION TOTALS
 let positiveNumbers = [];
 let negativeNumbers = [];
 
-let sum = positiveNumbers.reduce(function(a, b){
-    return a + b;
-}, 0);
+let posSum = 0;
+let negSum = 0;
 
 
-// Add transaction
+
+// ADD TRANSACTION, INCOME AND EXPENSES
 function addTransaction() {
     const list = document.createElement('div');
     list.classList.add('item');
@@ -27,12 +30,25 @@ function addTransaction() {
     items.appendChild(list);
 
     if(amount.value.includes('-')) {
-        list.classList.add('negative-item')
-    } else {
+        list.classList.add('negative-item');
+        negativeNumbers.push(amount.value);
+        const negNumArr = negativeNumbers.map((n) => {
+            return parseInt(n, 10);
+        })
+         negSum = negNumArr.reduce(function(a, b){
+            return a + b;
+        }, 0);
+    } 
+    
+    else {
         list.classList.add('positive-item');
         positiveNumbers.push(amount.value);
-        console.log(positiveNumbers);
-        console.log(sum);
+        const posNumArr = positiveNumbers.map((n) => {
+            return parseInt(n, 10);
+        })
+         posSum = posNumArr.reduce(function(a, b){
+            return a + b;
+        }, 0);
     }
 
     clearFields();
@@ -40,13 +56,11 @@ function addTransaction() {
 
 }
 
-
+// DISPLAY POSITIVE AND NEGATIVE SUM IN UI
 function calculation() {
-    let incomeBal = document.createElement('h3');
-    incomeBal.classList.add('income-bal', 'bal')
-
-        incomeBal.innerHTML = `${sum}`;
-        income.appendChild(incomeBal)
+    incomeBal.textContent = posSum;
+    expenseBal.textContent = negSum;
+    balance.textContent = posSum + negSum;
 }
 
 function clearFields() {
