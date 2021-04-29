460ccecc2fc820df0de8eecd9dba79c41bcc9170
Rabie <robienasser13@gmail.com>
2021-04-29 13:50:13 -0400

    form validator complete

--- Projects Course/1 Form Validator/app.js
+++ Projects Course/1 Form Validator/app.js
@@ -3,58 +3,70 @@ const email = document.querySelector('.email')
 const password = document.querySelector('.password')
 const confirmPassword = document.querySelector('.confirm-password')
 const submit = document.querySelector('button')
+const small = document.querySelectorAll('small')
 
 
 function createUserName() {
     re = /^(\w{1})((\w|\W){2,15})$/;
+    const error = document.querySelector('.username-error')
 
     if(!re.test(userName.value)) {
-        console.log('Username must be between 3 and 15 characters')
         userName.classList.add('invalid')
+        error.style.display = 'flex';
     } else {
-        console.log('success')
+        userName.classList.add('valid')
+        error.style.display = 'none'
     }
 }
 
 
 function createEmail() {
     re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+    const error = document.querySelector('.email-error')
 
     if(!re.test(email.value)) {
-        console.log('Please enter a valid email address')
         email.classList.add('invalid')
+        error.style.display = 'flex'
     } else {
-        console.log('success')
+        email.classList.add('valid')
+        error.style.display = 'none'
     }
 }
 
 
 function createPassword() {
     re = /^(?=.*\d)(?=.*\W)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z\W]{8,15}$/;
+    const error = document.querySelector('.password-error')
 
     if(password.value.length < 8) {
-       console.log('password must be at least 8 characters')
        password.classList.add('invalid')
+       error.textContent = 'password must be at least 8 characters'
+       error.style.display = 'flex'
     } else if(password.value.length > 15) {
-        console.log('password must not exceed 15 characters')
         password.classList.add('invalid')
+        error.textContent = 'Password must not exceed 15 characters'
+        error.style.display = 'flex'
     } else {
         if(!re.test(password.value)) {
-            console.log('Password must contain at least 1 uppercase letter, 1 number, and 1 special character')
             password.classList.add('invalid')
+            error.style.display = 'flex'
         } else {
-            console.log('success')
+            password.classList.add('valid')
+            error.style.display = 'none'
         }
     }
 }
 
 
 function checkPassword() {
-    if(password.value !== confirmPassword.value) {
-        console.log('passwords do not match')
+    const error = document.querySelector('.confirm-error')
+
+    if(password.value !== confirmPassword.value || confirmPassword.value === '' || password.classList.contains('invalid')) {
         confirmPassword.classList.add('invalid')
+        error.style.display = 'flex'
     } else {
-        console.log('success')
+        confirmPassword.classList.add('valid')
+        error.style.display = 'none'
     }
 }
 
