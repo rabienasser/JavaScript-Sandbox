5868947da8966bc68300abbeef8d6ac89066adc2
Rabie <robienasser13@gmail.com>
2021-05-04 18:35:11 -0400

    draggable functionality

--- Projects Course/19 Sortable List/app.js
+++ Projects Course/19 Sortable List/app.js
@@ -57,5 +57,37 @@ function addEventListeners() {
 
 
 function dragStart() {
-    
+    dragStartIndex = +this.closest('li').getAttribute('data-index');
+}
+
+
+function dragEnter() {
+    this.classList.add('over');
+}
+
+
+function dragLeave() {
+    this.classList.remove('over')
+}
+
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+
+function dragDrop() {
+    const dragEndIndex = +this.getAttribute('data-index');
+    swapItems(dragStartIndex, dragEndIndex)
+
+    this.classList.remove('over')
+}
+
+
+function swapItems(fromIndex, toIndex) {
+    const itemOne = listItems[fromIndex].querySelector('.draggable');
+    const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+    listItems[fromIndex].appendChild(itemTwo);
+    listItems[toIndex].appendChild(itemOne);
 }
