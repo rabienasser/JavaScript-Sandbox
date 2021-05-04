d456a870a87e2e377988ea6f64965d82627cf22b
Rabie <robienasser13@gmail.com>
2021-05-04 14:27:37 -0400

    draggable-list-styling

--- Projects Course/19 Sortable List/app.js
+++ Projects Course/19 Sortable List/app.js
@@ -13,4 +13,24 @@ createList();
 // insert list items into DOM
 function createList() {
     [...largestCities]
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((city, index) => {
+        const listItem = document.createElement('li')
+        
+        listItem.setAttribute('data-index', index);
+
+        listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+            <p class="city-name">${city}</p>
+            <i class="fas fa-grip-lines"></i>
+        </div>
+        `;
+
+        listItems.push(listItem);
+
+        draggableList.appendChild(listItem)
+    })
 }
