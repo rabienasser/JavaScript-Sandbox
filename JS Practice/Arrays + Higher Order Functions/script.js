26c97f80d48a12b25f89c6a72f01332b7fba1c62
Rabie <robienasser13@gmail.com>
2021-06-06 17:21:08 -0400

    array method practice

--- JS Practice/Arrays + Higher Order Functions/script.js
+++ JS Practice/Arrays + Higher Order Functions/script.js
@@ -15,6 +15,14 @@ const companies = [
 
 
 
+//   PUSH, POP, SHIFT, UNSHIFT
+// letters.push('f')  //['a','b','c','d','e','f']
+// letters.pop()  //['a','b','c','d',]
+// letters.unshift(1)  //[1,'a','b','c','d','e']
+// letters.shift()  //['b','c','d','e']
+// console.log(letters)
+
+
 
 //   SLICE
 //   console.log(letters.slice(2))  //Returns ['c', 'd', 'e']
@@ -43,8 +51,49 @@ const companies = [
 
 
 
+// INDEXOF
+// const arr = [1, 2, 3, 4, 5]
+// arr.indexOf(3) //returns 2
+
+
+
+// INCLUDES (checks if array contains item and returns boolean)
+// const arr = [1, 2, 3, 4, 5]
+// arr.includes(2)  //returns true
+
+
+
+// SOME (checks if array contains *any instance* and returns boolean)
+// const numberArr = [2, 15, 45, -19, 87, -93]
+// const someNum = numberArr.some(num => num < 0) //Does numberArr contain ANY values less than 0? *true*
+// console.log(someNum)
+
+
+
+// EVERY (checks if array contains *every instance* and returns boolean)
+// const numberArr = [2, 15, 45, -19, 87, -93]
+// const someNum = numberArr.every(num => num < 0) //Does numberArr contain ALL values less than 0? *false*
+// console.log(someNum)
+
+
+
 // JOIN
 // console.log(letters.join(' - '))
+
+
+
+// FIND (returns first instance in array that meets defined criteria) SIMILAR TO FILTER BUT DOES NOT RETURN NEW ARRAY
+// const numberArr = [2, 15, 45, -19, 87, -93]
+// const firstNegNumber = numberArr.find(num => num < 0)
+// console.log(firstNegNumber)  // returns -19
+// const company = companies.find(comp => comp.name === 'Company Six')
+// console.log(company)
+
+
+
+// FIND INDEX
+// const startDate = companies.findIndex(comp => comp.start === 1989)
+// console.log(startDate)
 
 
 
@@ -124,6 +173,24 @@ const companies = [
 // // new array of company category, all capital
 // const uppercaseCategory = companies.map(company => company.category.toUpperCase())
 // console.log(uppercaseCategory)
+
+
+
+// FLAT (takes nested arrays in returns one large array)
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8]
+// console.log(arr.flat());
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8]
+// console.log(arrDeep.flat(2))
+
+
+ 
+// FLAT MAP (combines flat() and map() into one method)
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8]
+// const test = arrDeep
+// .flat(2)
+// .flatMap(num => num + 2)
+// .reduce((total, num) => total + num, 0)
+// console.log(test)
 
 
 
@@ -259,3 +326,67 @@ const caclAverageHumanAgeChained = (ages) => {
 }
 
 caclAverageHumanAgeChained([5, 2, 4, 1, 15, 8, 3])
+
+
+
+
+// Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+// Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+// Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+// 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+// 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+// 4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+// 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+// 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+// 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+// HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+// HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+// TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+// Step 1
+dogs.forEach(dog => dog.recFoodPortion = `${(dog.weight ** 0.75) * 28}`)
+console.log(dogs)
+
+// Step 2
+const sarahsDog = dogs.find(dog => dog.owners.includes("Sarah")) 
+console.log(sarahsDog)
+console.log(`Sarah's dog is eating too ${sarahsDog.curFood > sarahsDog.recFoodPortion ? "much" : "little"}`)
+
+// Step 3
+const ownersEatTooMuch = dogs  
+.filter(dog => dog.curFood > dog.recFoodPortion)
+.flatMap(dog => dog.owners)
+
+const ownersEatTooLittle = dogs
+.filter(dog => dog.curFood < dog.recFoodPortion)
+.flatMap(dog => dog.owners)
+
+console.log(ownersEatTooMuch, ownersEatTooLittle)
+
+// Step 4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eats too much!`)
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eats too little!`)
+
+// Step 5
+console.log(dogs.some(dog => dog.curFood == dog.recFoodPortion))  //returns false
+
+// Step 6
+console.log(dogs.some(dog => dog.curFood > (dog.recFoodPortion * .90) && dog.curFood < (dog.recFoodPortion * 1.10)))  //returns true
+
+// Step 8
+const sortedDogs = dogs
+.slice()
+.sort((a, b) => a.recFoodPortion - b.recFoodPortion)
+// .map(dog => dog.recFoodPortion)
+// .sort((a, b) => a - b)
+console.log(sortedDogs)
